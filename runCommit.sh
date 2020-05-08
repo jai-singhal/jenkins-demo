5db88893ad1589e21c5867476ea714683371534b
Jai Singhal <jaisinghal48@gmail.com>
2020-05-08 10:38:02 +0530

    update script

--- runCommit.sh
+++ runCommit.sh
@@ -1,8 +1,16 @@
-python3 add.py 1 4
+echo "Run add.py with arg 6, 4"
+python3 add.py 6 4
+echo "-----------------------------"
 
+echo "Run add.cpp with arg 5, 97"
 g++ add.cpp -o "add"
 ./add 5 97
+echo "-----------------------------"
 
+echo "Execute run all files uing python which uses subprocess"
 python3 runAllfiles.py
+echo "-----------------------------"
 
+echo "Run add.sh with arg 65, 21"
 sh add.sh 65 21
+echo "-----------------------------"
